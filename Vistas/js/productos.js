2881645c7798a7e631974aef419ae30d08867f73
José Rondón <joserondon0319@gmail.com>
2024-07-02 21:53:20 -0500

    Modificación productos.js

--- Vistas/js/productos.js
+++ Vistas/js/productos.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
-  /* ===========================
+  /* ============================
   MOSTRANDO PRODUCTO
-  =========================== */
+  ============================ */
   function mostrarProductos() {
     $.ajax({
       url: "ajax/Producto.ajax.php",
